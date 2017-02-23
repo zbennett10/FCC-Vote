861007d1a4ab484403f0f9cad145ed00fa2eb944
Zachary Bennett <zbennett10@hotmail.com>
2017-02-23 10:03:46 -0500

    configured mongo connection

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const route = require('./routes');
 
 const app = express();
+
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/votedb`,
+                function(error) {
+                  if(error) console.log("Error connecting to mongo", error);
+                  console.log("Connected to mongo");
+                });
+
 
 app.use(bodyParser.json()); //user body parser middleware
 route(app); //connect server to routes
